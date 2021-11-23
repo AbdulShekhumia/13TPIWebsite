72a83b540390db74612e96ec8c5db67c6d188c2e
Abdul Shekhumia <ac98695@avcol.school.nz>
2021-11-23 22:51:37 +1300

    Successfully implemented email smtp on contact form in index page.

--- 13TPIWebsite/Controllers/HomeController.cs
+++ 13TPIWebsite/Controllers/HomeController.cs
@@ -7,6 +7,8 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Net;
+using System.Net.Mail;
 
 namespace _13TPIWebsite.Controllers
 {
@@ -20,6 +22,7 @@ namespace _13TPIWebsite.Controllers
             _logger = logger;
         }
         [AllowAnonymous]
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
@@ -84,6 +87,63 @@ namespace _13TPIWebsite.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult Index(ContactMail contactMail)
+        {
+            if (!ModelState.IsValid) return View();
+
+            try
+            {
+                MailMessage mail = new MailMessage();
+                // you need to enter your mail address
+                mail.From = new MailAddress("globalclimatevision@gmail.com");
+
+                //To Email Address - your need to enter your to email address
+                mail.To.Add("globalclimatevision@gmail.com");
+
+                mail.Subject = contactMail.Name;
+
+                //you can specify also CC and BCC - i will skip this
+                //mail.CC.Add("");
+                //mail.Bcc.Add("");
+
+                mail.IsBodyHtml = true;
+
+                string content = "Name : " + contactMail.Name;
+                content += "<br/> Message : " + contactMail.Message;
+                content += "<br/> Email : " + contactMail.Email;
+
+                mail.Body = content;
+
+
+                //create SMTP instant
+
+                //you need to pass mail server address and you can also specify the port number if you required
+                SmtpClient smtpClient = new SmtpClient("smtp.gmail.com");
+
+                //Create nerwork credential and you need to give from email address and password
+                NetworkCredential networkCredential = new NetworkCredential("globalclimatevision@gmail.com", "QWERTY45");
+                smtpClient.UseDefaultCredentials = false;
+                smtpClient.Credentials = networkCredential;
+                smtpClient.Port = 25; // this is default port number - you can also change this
+                smtpClient.EnableSsl = true; // if ssl required you need to enable it
+                smtpClient.Send(mail);
+
+                ViewBag.Message = "Mail Send";
+
+                // now i need to create the from 
+                ModelState.Clear();
+
+            }
+            catch (Exception ex)
+            {
+                //If any error occured it will show
+                ViewBag.Message = ex.Message.ToString();
+            }
+            return RedirectToAction("ContactSubmitted");
         }
     }
 }
---
+++ 13TPIWebsite/Models/ContactMail.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace _13TPIWebsite.Models
+{
+    public class ContactMail
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string Message { get; set; }
+    }
+}
